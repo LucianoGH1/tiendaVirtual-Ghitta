0333c74b2b61d657c090a02720f735e3a38bfc79
LucianoGH1 <luciano.ghitta@gmail.com>
2022-07-01 17:27:56 -0300

    agregue navBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+
+import logo from '../logo.png';
+const NavBar = () => {
+    return (  
+        <navbar className='navbar navbar-expand-lg navbar-light bg-light justify-content-between'>
+            <div className='navbar-brand d-flex align-middle'>
+                <img src={logo}/>
+                <h1>Shop</h1>
+            </div>
+            <ul className='navbar-nav w-50 d-flex justify-content-around'>
+                <li className='nav-item'><a className='nav-link' href='#'>Home</a></li>
+                <li><a className='nav-link' href='#'>Products</a></li>
+                <li><a className='nav-link' href='#'>About us</a></li>
+            </ul>
+        </navbar>
+    );
+}
+ 
+export default NavBar;
